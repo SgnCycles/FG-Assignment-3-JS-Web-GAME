cbdc247a6e65a657d50ab3ff2031e91994520d83
SgnCycles <signe.bekere@gmail.com>
2025-11-27 20:46:02 +0100

    fixed action messages

--- javascript/instruction.js
+++ javascript/instruction.js
@@ -1,21 +1,18 @@
-import {currentCountry} from "./gameplay.js";
-import {hintPenalty} from "./gameplay.js";
-import {hintUsed} from "./gameplay.js";
-import {livesGenerator} from "./gameplay.js";
+import {currentCountry, hintPenalty, hintUsed, livesGenerator, showCountry} from "./gameplay.js";
 
 const guessInput = document.querySelector(".typing-input");
 let rulesButton = document.querySelector(".rules-button");
 let rulesContent = document.querySelector(".instruction");
-let countryGirl = document.querySelector(".country-girl-image-container");
+let actionImage = document.querySelector(".action-image-container");
 let hintButton = document.querySelector(".hint-button");
 let resetButton = document.querySelector(".reset-button");
 
 rulesButton.addEventListener("click", () => {
-  if (countryGirl.classList.contains("active")) {
-    countryGirl.classList.remove("active");
+  if (actionImage.classList.contains("active")) {
+    actionImage.classList.remove("active");
     rulesContent.classList.add("active");
   } else {
-    countryGirl.classList.add("active");
+    actionImage.classList.add("active");
     rulesContent.classList.remove("active");
   }
   guessInput.focus();
@@ -27,20 +24,19 @@ hintButton.addEventListener("click", () => {
   guessInput.focus();
   if (hintUsed) return; 
   hintPenalty();
-  hintUsed = true;
+  // hintUsed = true;
 })
 
-// needs to be fixed, does not work with lives now
 resetButton.addEventListener("click", () => {
   guessInput.focus();
   const livesContainer = document.querySelector(".life-score");
-  let attemptsScore = document.querySelector("#attempts-counter");
-  let scoreCounter = document.querySelector("#score-counter");
+  const attemptsScore = document.querySelector("#attempts-counter");
+  const scoreCounter = document.querySelector("#score-counter");
+
   attemptsScore.textContent = 5;
   scoreCounter.textContent = 0;
-  // lives = 3; vajag so, ja ir apaksa funkcija?
-  attempts = 5;
-  score = 0;
   livesContainer.innerHTML = "";
+
   livesGenerator();
+  showCountry();
 })
